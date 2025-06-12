2cfeb21bb0035ab9606aa09be84c11dfd1ad6f97
PARTHIBAN-007 <157013976+PARTHIBAN-007@users.noreply.github.com>
2025-06-12 08:15:05 +0530

    Git Replace check

--- src/domain/prompts.py
+++ src/domain/prompts.py
@@ -13,6 +13,6 @@ if you found that you have adequate information to answer user question you can 
 """
 
 
-CONTEXT_SUMMARY_PROMPT = ""
+CONTEXT_SUMMARY_PROMPT = "Summarize the content properly to make it proper context and provide more importance to numbers.don't negelct the importance of importance provide a quick summary for the qiven text content"
 
 EXTEND_CONTEXT_SUMMARY_PROMPT = ""
